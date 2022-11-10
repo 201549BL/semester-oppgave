814e42daa016e8930cec96ecb00be1eb4c883f42
Eirik Bjørndal <eirik.bjorndal@gmail.com>
2022-11-10 17:00:13 +0100

    redesigned cta, cta dynamic size, sources target

--- main.js
+++ main.js
@@ -17,3 +17,27 @@ const onLoaded = (e) => {
   );
 };
 document.addEventListener("DOMContentLoaded", () => onLoaded());
+
+// Remove outline until needed by tabbing
+const root = document.querySelector("html");
+root.classList.add("hide-outline");
+
+document.addEventListener("keydown", (e) => {
+  if (e.key !== "Tab") return;
+
+  root.classList.remove("hide-outline");
+});
+
+// Set height of call-to-action dynamically
+const cta = document.querySelector("#call-to-action");
+const ctaRoot = document.querySelector(".call-to-action");
+const ctaFillers = document.querySelectorAll("[data-cta-filler='true'");
+
+const resizeCta = () => {
+  ctaRoot.style.height = cta.clientHeight + "px";
+  ctaFillers.forEach((cf) => (cf.style.height = cta.clientHeight + "px"));
+};
+
+window.addEventListener("resize", resizeCta);
+
+resizeCta();
